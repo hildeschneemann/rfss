7b8a55d1cbe5994bd481094b5d76ed512adc8655
alxsimon <alexis.simon@normalesup.org>
2017-08-24 13:49:03 +0200

    Simple plot function for one generation

--- R/plotting.R
+++ R/plotting.R
@@ -1,10 +1,24 @@
-plot_timestep <- function(data, timestep = 1) {
-  allele_freq <- data$alleleFreq[,,timestep]
-  allele_freq.l <- melt(allele_freq, id.vars = "deme")
-  ggplot(allele_freq.l, aes(x = deme, y = value, colour = variable)) +
+plot_timestep <- function(data, gen = 1, message = T) {
+  # find index of generation or closest
+  if (gen %in% data$savedGen) {
+    igen <- which(data$savedGen == gen)
+  } else {
+    igen <- which.min(abs(data$savedGen - gen))
+    gen <- data$savedGen[igen]
+    if (message)
+      cat("Plotting the closest generation available: ", gen)
+  }
+  allele_freq <- melt(data$alleleFreq[,,igen],
+                        varnames = c("deme","locus"), value.name = "freq")
+  allele_freq$locus <- as.factor(allele_freq$locus)
+
+  cols <- diverge_hcl(dim(data$alleleFreq)[2])
+
+  ggplot(allele_freq, aes(x = deme, y = freq, colour = locus)) +
     geom_line() +
     scale_colour_manual(values = cols) +
     ylim(c(0,1)) +
+    labs(title = paste("generation", gen)) +
     theme_classic() +
     theme(legend.position = "none")
 }
