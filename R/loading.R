a1ac1824818e1b759a596a298426b77da895d105
alxsimon <alexis.simon@normalesup.org>
2017-08-24 11:55:35 +0200

    provide a dataset

---
+++ R/loading.R
@@ -0,0 +1,17 @@
+load_h5 <- function(file, dataset = NULL) {
+  if (is.null(dataset)) {
+    # load everything
+    res <- h5read(file, name = "/")
+    return(res)
+  } else {
+    # load the specified dataset
+    if (dataset %in% h5ls(file)$name) {
+      res <- h5read(file, name = dataset)
+    } else {
+      warning("The dataset name you have specified does not exist.\n",
+              "Try one of the following:\n",
+              paste(h5ls(file)$name, collapse = "\n"))
+    }
+    return(res)
+  }
+}
